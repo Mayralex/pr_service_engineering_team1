ce4c1103801c311d771755a3ef3937b36c5a1827
Alexander <alex.mayr.mobil@gmail.com>
2023-11-02 09:42:24 +0100

    sanity checks for function

--- backend/ADR_Viewer/src/main/java/com/serviceEngineering/ADR_Viewer/service/GithubService.java
+++ backend/ADR_Viewer/src/main/java/com/serviceEngineering/ADR_Viewer/service/GithubService.java
@@ -1,15 +1,17 @@
 package com.serviceEngineering.ADR_Viewer.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.serviceEngineering.ADR_Viewer.ADRParser;
 import com.serviceEngineering.ADR_Viewer.entity.ADR;
 import com.serviceEngineering.ADR_Viewer.entity.RestResponse;
+import com.serviceEngineering.ADR_Viewer.exceptions.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
 
 @Service
 public class GithubService {
@@ -27,14 +29,14 @@ public class GithubService {
     /***
      * TODO: create search methode in order to search for ADRs inside a project ---> Goal is that, the user does not have to specify
      *  the path to the folder
-     * @param owner: Github Username of the person created the Github repository
-     * @param repoName: Name of the repository stored in Github
-     * @param filePath: Path to the directory where to ADRs are stores ---> To be decommissioned
+     * @param owner: GitHub Username of the person created the GitHub repository
+     * @param repoName: Name of the repository stored in GitHub
+     * @param directoryPath: Path to the directory where to ADRs are stores ---> To be decommissioned
      * @param branch: Github branch
-     * @return: Array of RestResponse.class in JSON
+     * @return: Array of RestResponse. Class in JSON
      */
-    public RestResponse[] fetchRepositoryContent(String owner, String repoName, String filePath, String branch) {
-        String apiUrl = String.format("%s/repos/%s/%s/contents/%s?ref=%s", githubApiUrl, owner, repoName, filePath, branch);
+    public RestResponse[] fetchRepositoryContent(String owner, String repoName, String directoryPath, String branch) {
+        String apiUrl = String.format("%s/repos/%s/%s/contents/%s?ref=%s", githubApiUrl, owner, repoName, directoryPath, branch);
 
         ResponseEntity<RestResponse[]> responseEntity =
                 restTemplate.getForEntity(apiUrl, RestResponse[].class);
@@ -42,10 +44,12 @@ public class GithubService {
     }
 
     public String fetchADRFile(String owner, String repoName, String filePath, String branch) {
-        String apiUrl = String.format("%s/repos/%s/%s/contents/%s?ref=%s", githubApiUrl, owner, repoName, filePath, branch);
-        ResponseEntity<RestResponse> responseEntity =
-                restTemplate.getForEntity(apiUrl, RestResponse.class);
-        return restTemplate.getForEntity(responseEntity.getBody().getDownload_url(), String.class).getBody();
+        if (filePath.endsWith(".md")) {
+            String apiUrl = String.format("%s/repos/%s/%s/contents/%s?ref=%s", githubApiUrl, owner, repoName, filePath, branch);
+            ResponseEntity<RestResponse> responseEntity =
+                    restTemplate.getForEntity(apiUrl, RestResponse.class);
+            return restTemplate.getForEntity(Objects.requireNonNull(responseEntity.getBody()).getDownload_url(), String.class).getBody();
+        } else throw new ServiceException("Path not pointing to markdown file", filePath);
     }
 
     public String parseADRFileToHTML(String owner, String repoName, String filePath, String branch) {
@@ -53,12 +57,10 @@ public class GithubService {
         return ADRParser.convertMarkdownToHTML(markdown);
     }
 
-    public Object parseADRFile(String owner, String repoName, String filePath, String branch) throws JsonProcessingException {
+    public Object parseADRFile(String owner, String repoName, String filePath, String branch) {
         String markdown = fetchADRFile(owner, repoName, filePath, branch);
         String html = ADRParser.convertMarkdownToHTML(markdown);
         ADR adr = ADRParser.convertHTMLToADR(html);
-
-        //return new ObjectMapper().writeValue(adr);
         return new ResponseEntity<Object>(adr, HttpStatus.OK);
     }
 
