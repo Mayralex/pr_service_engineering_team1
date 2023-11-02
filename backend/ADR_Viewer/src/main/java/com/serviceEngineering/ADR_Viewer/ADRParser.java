2238e9599728d691223ba761f2d9b87f86a3f675
Alexander <alex.mayr.mobil@gmail.com>
2023-11-02 12:11:58 +0100

    refactoring linkExtraction for better readability

--- backend/ADR_Viewer/src/main/java/com/serviceEngineering/ADR_Viewer/ADRParser.java
+++ backend/ADR_Viewer/src/main/java/com/serviceEngineering/ADR_Viewer/ADRParser.java
@@ -29,7 +29,6 @@ public class ADRParser {
     public static ADR convertHTMLToADR(String html) {
         Document document = Jsoup.parse(html);
         ADR adr = new ADR();
-
         adr.setTitle(Objects.requireNonNull(document.selectFirst("h1")).textNodes().get(0).toString());
         adr.setContext(extractSectionText(document, "Context"));
         adr.setDecision(extractSectionText(document, "Decision"));
@@ -66,18 +65,9 @@ public class ADRParser {
 
         if (h2 != null) {
             Element nextSibling = h2.nextElementSibling();
-
             while (nextSibling != null && !nextSibling.tagName().equals("h2")) {
                 if (nextSibling.tagName().equals("ul")) {
-                    Elements listItems = nextSibling.select("li");
-                    for (Element listItem : listItems) {
-                        Element link = listItem.selectFirst("a");
-                        if (link != null) {
-                            String linkText = link.text();
-                            String linkHref = link.attr("href");
-                            linksMap.put(linkText, linkHref);
-                        }
-                    }
+                    extractLinksFromList(linksMap, nextSibling);
                 }
                 nextSibling = nextSibling.nextElementSibling();
             }
@@ -85,5 +75,18 @@ public class ADRParser {
 
         return linksMap;
     }
+
+    private static void extractLinksFromList(Map<String, String> linksMap, Element ulElement) {
+        Elements listItems = ulElement.select("li");
+        for (Element listItem : listItems) {
+            Element link = listItem.selectFirst("a");
+            if (link != null) {
+                String linkText = link.text();
+                String linkHref = link.attr("href");
+                linksMap.put(linkText, linkHref);
+            }
+        }
+    }
+
 }
 
