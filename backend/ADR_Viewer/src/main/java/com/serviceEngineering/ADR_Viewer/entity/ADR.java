3eb804c906bddba3a5b3765d0492c48ecd126eea
Alexander <alex.mayr.mobil@gmail.com>
2023-11-03 15:20:44 +0100

    table structure for caching

--- backend/ADR_Viewer/src/main/java/com/serviceEngineering/ADR_Viewer/entity/ADR.java
+++ backend/ADR_Viewer/src/main/java/com/serviceEngineering/ADR_Viewer/entity/ADR.java
@@ -1,5 +1,6 @@
 package com.serviceEngineering.ADR_Viewer.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.serviceEngineering.ADR_Viewer.div.Status;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -12,18 +13,33 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(name = "ADR")
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ADR {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "id", nullable = false)
+    @Column(name = "ID", nullable = false)
     private int id;
 
+    @Column(name = "Title", nullable = false)
     private String title;
+
+    @Column(name = "Context", length =  999999)
     private String context;
+
+    @Column(name = "Decision", length =  999999)
     private String decision;
+
+    @Column(name = "Status")
     private Status status;
+
+    @Column(name = "Consequences", length =  999999)
     private String consequences;
+
+    @Column(name = "Artifacts", length =  999999)
     private String artifacts; //TODO: find correct type
+
+    @Column(name = "Relations", length =  999999)
     private String relations; //TODO: find correct type ----> probably custom type Relation
 
 }
