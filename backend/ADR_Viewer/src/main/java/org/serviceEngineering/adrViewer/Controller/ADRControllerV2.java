473deb252a6c4c17f4fe96f8c7e87fc6e7fac67c
Alexander <alex.mayr.mobil@gmail.com>
2023-11-03 22:11:12 +0100

    Added JavaDoc and changed return value for getAllADRs

--- backend/ADR_Viewer/src/main/java/org/serviceEngineering/adrViewer/Controller/ADRControllerV2.java
+++ backend/ADR_Viewer/src/main/java/org/serviceEngineering/adrViewer/Controller/ADRControllerV2.java
@@ -13,8 +13,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -22,12 +22,27 @@ import java.util.List;
 public class ADRControllerV2 {
 
     private final ADRService adrService;
+    private final RestTemplate restTemplate;
     private final Logger log = LoggerFactory.getLogger(ADRControllerV2.class);
     @Autowired
-    public ADRControllerV2(ADRService adrService) {
+    public ADRControllerV2(RestTemplate restTemplate,
+                           ADRService adrService) {
+        this.restTemplate = restTemplate;
         this.adrService = adrService;
     }
 
+    /**
+     * Controller method for retrieving an ADR by its unique identifier.
+     * <p>
+     * This endpoint allows you to fetch a specific ADR by providing its unique ID. If the ADR with the
+     * specified ID is found in the database, it will be returned with an HTTP status of 200 (OK).
+     * If the ADR is not found or if there's a service exception, it will return an appropriate HTTP
+     * status code along with the error message.
+     *
+     * @param id The unique identifier of the ADR to retrieve.
+     * @return ResponseEntity containing the ADR object if found, or an error message with an appropriate
+     * HTTP status code if the ADR is not found or if there's a service exception.
+     */
     @GetMapping(value = "/getADR")
     public ResponseEntity<Object> getADR(@RequestParam long id) {
         try{
@@ -38,14 +53,31 @@ public class ADRControllerV2 {
         }
     }
 
+    /**
+     * Controller method for retrieving ADRs from a repository.
+     *
+     * This endpoint allows you to fetch ADRs from a specific repository by specifying the repository owner,
+     * repository name, directory path, and branch. It first checks if there are any ADRs in the memory,
+     * and if so, it returns them. If not, it fetches ADRs from the specified repository using a REST API call
+     * and saves them in the database for future use.
+     *
+     * @param repoOwner The owner of the repository where ADRs are stored.
+     * @param repoName The name of the repository where ADRs are stored.
+     * @param directoryPath The path to the directory within the repository where ADRs are located.
+     * @param branch The branch in the repository from which ADRs should be fetched.
+     * @return ResponseEntity containing an array of ADR objects if ADRs are found, or an empty array if no ADRs are available.
+     */
     @GetMapping(value = "/getAllADRs")
-    public List<ADR> getAllADRs(
+    public ResponseEntity<ADR[]> getAllADRs(
             @RequestParam String repoOwner,
             @RequestParam String repoName,
             @RequestParam String directoryPath,
             @RequestParam String branch
     ) {
-        ArrayList<ADR> result = new ArrayList<>();
+        List<ADR> result = adrService.getAll();
+        if (!result.isEmpty()) return restTemplate.getForEntity(
+                result.toString(),
+                ADR[].class);
         RestResponse[] list = adrService.fetchRepositoryContent(repoOwner, repoName, directoryPath, branch);
 
         for (RestResponse response : list) {
@@ -55,7 +87,9 @@ public class ADRControllerV2 {
             result.add(adrService.parseADRFile(repoOwner, repoName, filepath, branch));
         }
         adrService.saveAll(result);
-        return result;
+        return restTemplate.getForEntity(
+                result.toString(),
+                ADR[].class);
     }
 
 
