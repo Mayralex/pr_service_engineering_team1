9f0139c8fabb6bcb53f27154f4046cbb7b04a618
Alexander <alex.mayr.mobil@gmail.com>
2023-11-03 15:20:26 +0100

    added api version into request mapping

--- backend/ADR_Viewer/src/main/java/com/serviceEngineering/ADR_Viewer/Controller/ADRController.java
+++ backend/ADR_Viewer/src/main/java/com/serviceEngineering/ADR_Viewer/Controller/ADRController.java
@@ -6,10 +6,12 @@ import com.serviceEngineering.ADR_Viewer.entity.RestResponse;
 import com.serviceEngineering.ADR_Viewer.service.ADRService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class ADRController {
 
     @Autowired
