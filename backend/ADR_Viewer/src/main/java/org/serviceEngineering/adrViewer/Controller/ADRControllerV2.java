05c32e7211869b24f8777091721d19c7340440c7
Alexander <alex.mayr.mobil@gmail.com>
2023-11-03 21:44:47 +0100

    new Endpoint: get all ADRs in directory, parse and store them in memory

--- backend/ADR_Viewer/src/main/java/org/serviceEngineering/adrViewer/Controller/ADRControllerV2.java
+++ backend/ADR_Viewer/src/main/java/org/serviceEngineering/adrViewer/Controller/ADRControllerV2.java
@@ -1,8 +1,11 @@
 package org.serviceEngineering.adrViewer.Controller;
 
 import org.serviceEngineering.adrViewer.entity.ADR;
+import org.serviceEngineering.adrViewer.entity.RestResponse;
 import org.serviceEngineering.adrViewer.exceptions.ServiceException;
 import org.serviceEngineering.adrViewer.service.ADRService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +14,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/api/v2")
 public class ADRControllerV2 {
 
     private final ADRService adrService;
+    private final Logger log = LoggerFactory.getLogger(ADRControllerV2.class);
     @Autowired
     public ADRControllerV2(ADRService adrService) {
         this.adrService = adrService;
@@ -29,6 +36,26 @@ public class ADRControllerV2 {
         } catch (ServiceException e){
             return new ResponseEntity<>(e, HttpStatus.NOT_FOUND);
         }
+    }
+
+    @GetMapping(value = "/getAllADRs")
+    public List<ADR> getAllADRs(
+            @RequestParam String repoOwner,
+            @RequestParam String repoName,
+            @RequestParam String directoryPath,
+            @RequestParam String branch
+    ) {
+        ArrayList<ADR> result = new ArrayList<>();
+        RestResponse[] list = adrService.fetchRepositoryContent(repoOwner, repoName, directoryPath, branch);
+
+        for (RestResponse response : list) {
+            if (!response.getType().equals("file")) continue;
+            String filepath = response.getPath();
+            log.info(filepath);
+            result.add(adrService.parseADRFile(repoOwner, repoName, filepath, branch));
+        }
+        adrService.saveAll(result);
+        return result;
     }
 
 
