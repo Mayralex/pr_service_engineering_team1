faaddc1fa623eea911a1b98c3bec5c66269a8c05
RonaldMoritz <ronald_moritz@yahoo.com>
2023-11-26 14:51:04 +0100

    #48: Put links into navbar.

--- frontend/adr-viewer-ui/src/app/components/nav/nav.component.ts
+++ frontend/adr-viewer-ui/src/app/components/nav/nav.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  // title on the left of navbar
+  title = "ADR-Viewer";
+
+  // list of items in the navbar, including route and name - can be added/removed here
+  navbarItems = [
+    ["/home", "Home"],
+    ["/listview", "ADR-List"],
+    ["", "Dashboard"]
+  ];
   constructor() { }
 
   ngOnInit(): void {
