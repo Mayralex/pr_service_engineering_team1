21e65d1212d47c088163fa15c3a947c14cce70c6
juliaweinreich <54713794+juliaweinreich@users.noreply.github.com>
2023-12-26 17:47:14 +0100

    #87: refactored dashboard-view, resolved bug when loading ADRs, introduced barchart and linechart component

--- frontend/adr-viewer-ui/src/app/app.module.ts
+++ frontend/adr-viewer-ui/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { AdrDetailViewComponent } from './components/adr-detail-view/adr-detail-
 import { FormsModule } from "@angular/forms";
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NgChartsModule } from "ng2-charts";
+import {BarchartComponent} from "./components/visualizations/barchart/barchart.component";
+import { LinechartComponent } from './components/visualizations/linechart/linechart.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,9 @@ import { NgChartsModule } from "ng2-charts";
     HomeComponent,
     NavComponent,
     AdrDetailViewComponent,
-    DashboardComponent
+    DashboardComponent,
+    BarchartComponent,
+    LinechartComponent
   ],
   imports: [
     BrowserModule,
