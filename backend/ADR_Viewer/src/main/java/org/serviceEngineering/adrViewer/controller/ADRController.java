8968428e89527323d2760fffaa59a8ede401a570
Alexander <alex.mayr.mobil@gmail.com>
2024-01-22 17:58:27 +0100

    Refactoring for better readability and adding missing javadocs for ADRController.java

--- backend/ADR_Viewer/src/main/java/org/serviceEngineering/adrViewer/controller/ADRController.java
+++ backend/ADR_Viewer/src/main/java/org/serviceEngineering/adrViewer/controller/ADRController.java
@@ -15,7 +15,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +23,7 @@ import java.util.List;
 
 @Tag(name = "ADR", description = "ADR Api")
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/v2/")
 public class ADRController {
 
     private final ADRService adrService;
@@ -32,6 +31,15 @@ public class ADRController {
     private final CommitHistoryClient commitHistoryClient;
     private final Logger log = LoggerFactory.getLogger(ADRController.class);
 
+    /**
+     * Constructor for the ADRController class.
+     * <p>
+     * Initializes the ADRController with the provided instances of ADRService, ImportService, and CommitHistoryClient.
+     *
+     * @param adrService          Service for managing Architectural Decision Records (ADRs).
+     * @param importService       Service for managing ADR imports.
+     * @param commitHistoryClient Client for retrieving commit history from a GitHub repository.
+     */
     @Autowired
     public ADRController(ADRService adrService, ImportService importService, CommitHistoryClient commitHistoryClient) {
         this.adrService = adrService;
@@ -40,8 +48,9 @@ public class ADRController {
     }
 
     /**
-     * Controller method for retrieving an ADR by its unique identifier.
-     * This endpoint allows you to fetch a specific ADR by providing its unique ID. If the ADR with the
+     * Controller method for retrieving an Architectural Decision Record (ADR) by its unique identifier.
+     * <p>
+     * This endpoint allows fetching a specific ADR by providing its unique ID. If the ADR with the
      * specified ID is found in the database, it will be returned with an HTTP status of 200 (OK).
      * If the ADR is not found or if there's a service exception, it will return an appropriate HTTP
      * status code along with the error message.
@@ -50,15 +59,13 @@ public class ADRController {
      * @return ResponseEntity containing the ADR object if found, or an error message with an appropriate
      * HTTP status code if the ADR is not found or if there's a service exception.
      */
-    @Operation(
-            summary = "Get single adr",
-            description = "Fetches single ADR from storage via ID")
+    @Operation(summary = "Get single ADR", description = "Fetches single ADR from storage via ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "successful operation"),
             @ApiResponse(responseCode = "404", description = "resource not found")
     })
-    @CrossOrigin(origins = "http://localhost:4200") // only allows access from our frontend
-    @GetMapping(value = "v2/adr/{id}")
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("adr/{id}")
     public ResponseEntity<Object> getADR(@PathVariable int id) {
         try {
             ADR adr = adrService.getADR(id);
@@ -69,89 +76,86 @@ public class ADRController {
     }
 
     /**
-     * Controller method for retrieving ADRs
-     * This endpoint allows you to fetch all ADRs
+     * Controller method for retrieving all Architectural Decision Records (ADRs).
+     * <p>
+     * This endpoint allows fetching a list of all ADRs. The ADRs are retrieved from the storage,
+     * and if successful, the list is returned with an HTTP status of 200 (OK).
      *
-     * @return List of ADRs from memory
+     * @return ResponseEntity containing a list of ADRs if the operation is successful.
      */
-    @Operation(
-            summary = "Get all adrs",
-            description = "Get a list of all ADRs")
+    @Operation(summary = "Get all ADRs", description = "Get a list of all ADRs")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "successful operation")
     })
-    @CrossOrigin(origins = "http://localhost:4200") // only allows access from our frontend
-    @GetMapping(value = "v2/getAllADRs", produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("getAllADRs")
     public ResponseEntity<Object> getAllADRs() {
         List<ADR> result = adrService.getAll();
-        log.info("List of ADRs consists of {} adrs", result.toArray().length);
+        log.info("List of ADRs consists of {} ADRs", result.size());
         return new ResponseEntity<>(result, HttpStatus.OK);
-        //TODO: error handling if not successful
     }
 
-
     /**
-     * Controller method for retrieving ADRs from a repository.
-     * This endpoint allows you to fetch ADRs from a specific repository
+     * Controller method for retrieving all Architectural Decision Records (ADRs) of the current project.
+     * <p>
+     * This endpoint allows fetching a list of all ADRs associated with the current project identified
+     * by the provided importTaskId. The ADRs are retrieved from the storage, and if successful, the list
+     * is returned with an HTTP status of 200 (OK).
      *
-     * @return List of ADRs from memory
+     * @param importTaskId The identifier of the import task representing the current project.
+     * @return ResponseEntity containing a list of ADRs if the operation is successful.
      */
-    @Operation(
-            summary = "Get all adrs of current project",
-            description = "Get a list of all ADRs in the current project")
+    @Operation(summary = "Get all ADRs of current project", description = "Get a list of all ADRs in the current project")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "successful operation")
     })
-    @CrossOrigin(origins = "http://localhost:4200") // only allows access from our frontend
-    @GetMapping(value = "v2/getAllADRsOfProject", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> getAllADRsOfCurrentProject(
-            @RequestParam int importTaskId
-    ) {
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("getAllADRsOfProject")
+    public ResponseEntity<Object> getAllADRsOfCurrentProject(@RequestParam int importTaskId) {
         List<ADR> result = adrService.getAllByProject(importTaskId);
-        log.info("List of ADRs consists of {} adrs", result.toArray().length);
+        log.info("List of ADRs consists of {} ADRs", result.size());
         return new ResponseEntity<>(result, HttpStatus.OK);
-        //TODO: error handling
     }
 
     /**
-     * Controller method for retrieving Architectural Decision Records (ADRs) by their status.
-     * This endpoint allows you to fetch ADRs from the database based on their status.
+     * Controller method for retrieving a list of Architectural Decision Records (ADRs) by status.
+     * <p>
+     * This endpoint allows fetching a list of ADRs from storage based on the provided status.
+     * If successful, the list is returned with an HTTP status of 200 (OK).
      *
      * @param status The status of ADRs to retrieve.
-     * @return ResponseEntity containing a list of ADRs filtered by the specified status with an HTTP status of 200 (OK).
+     * @return ResponseEntity containing a list of ADRs if the operation is successful.
      */
-    @Operation(
-            summary = "Get list of ADRs by status",
-            description = "Get a list of ADRs from storage with provided status")
+    @Operation(summary = "Get list of ADRs by status", description = "Get a list of ADRs from storage with provided status")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "successful operation")
     })
-    @CrossOrigin(origins = "http://localhost:4200") // only allows access from our frontend
-    @GetMapping(value = "v2/getByStatus", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> getByStatus(
-            @RequestParam String status
-    ) {
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("getByStatus")
+    public ResponseEntity<Object> getByStatus(@RequestParam String status) {
         List<ADR> result = adrService.getByStatus(status);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     /**
-     * Controller method for retrieving Architectural Decision Records (ADRs) by query, pageOffset and limit.
-     * This endpoint allows you to retrieve a number of ADRs based on pageOffset and limit and also considers filtering with query
+     * Controller method for retrieving a page of Architectural Decision Records (ADRs) by pageOffset and limit.
+     * <p>
+     * This endpoint allows retrieving a specified number of ADRs based on pageOffset and limit,
+     * and it also considers filtering with a query. If successful, the result is returned with
+     * an HTTP status of 200 (OK).
      *
-     * @param query searchText
-     * @param pageOffset offset of a page
-     * @param limit limit of ADRs per page
-     * @return ResponseEntity containing a list of ADRs
+     * @param importTaskId The identifier of the import task representing the current project.
+     * @param query        Search text to filter ADRs.
+     * @param pageOffset   Offset of the page.
+     * @param limit        Limit of ADRs per page.
+     * @return ResponseEntity containing a page of ADRs if the operation is successful.
      */
-    @Operation(
-            summary = "Get page of ADRs by pageOffset and limit",
-            description = "Get a page of ADRs from storage by pageOffset and limit")
+    @Operation(summary = "Get page of ADRs by pageOffset and limit", description = "Get a page of ADRs from storage by pageOffset and limit")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "successful operation")
     })
-    @CrossOrigin(origins = "http://localhost:4200") // only allows access from our frontend
-    @GetMapping(value = "v2/ADR", produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("ADR")
     public ResponseEntity<Object> getByPageOffsetAndLimit(
             @RequestParam int importTaskId,
             @RequestParam(required = false, defaultValue = "") String query,
@@ -164,20 +168,21 @@ public class ADRController {
 
     /**
      * Controller method for retrieving commit history for a specific file within a GitHub repository.
-     * This endpoint allows you to fetch commit history for a particular file in a GitHub repository
-     * @param importTaskId if og the current importTask (to get repoOwner, repoName and branch)
-     * @param filePath path of the file to the ADR
-     * @return a CommitDTO
-     * @throws IOException
+     * <p>
+     * This endpoint allows fetching commit history for a particular file in a GitHub repository,
+     * including the commit OID, committed date, commit message, and ADR status.
+     *
+     * @param importTaskId The identifier of the import task representing the current project.
+     * @param filePath      Path to the file related to the Architectural Decision Record (ADR).
+     * @return ResponseEntity containing the commit history information if the operation is successful.
+     * @throws IOException If an I/O exception occurs during the operation.
      */
-    @Operation(
-            summary = "Get commit history",
-            description = "Get the commit history (oid, committedDate, message, adr status)")
+    @Operation(summary = "Get commit history", description = "Get the commit history (oid, committedDate, message, ADR status)")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "successful operation")
     })
-    @CrossOrigin(origins = "http://localhost:4200") // only allows access from our frontend
-    @GetMapping(value = "v2/getHistory", produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("getHistory")
     public ResponseEntity<Object> getHistory(
             @RequestParam int importTaskId,
             @RequestParam String filePath
@@ -185,25 +190,24 @@ public class ADRController {
         ImportTask importTask = importService.getImportTask(importTaskId);
         Object result = commitHistoryClient.getHistory(importTask.getRepoOwner(), importTask.getRepoName(), filePath, importTask.getBranch());
         log.info(result.toString());
-
-        //CommitDTO latestCommitDTO = adrService.extractLatestCommit(result);
-        //return new ResponseEntity<>(latestCommitDTO, HttpStatus.OK);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     /**
-     * Clears the cache of repositories
+     * Controller method for clearing the cache of repositories.
+     * <p>
+     * This endpoint allows removing all objects from the cache associated with repositories.
+     * It performs cache clearance, and if successful, logs the information with an HTTP status of 200 (OK).
      */
-    @Operation(
-            summary = "Clear cache",
-            description = "Remove all objects from cache")
+    @Operation(summary = "Clear cache", description = "Remove all objects from cache")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "successful operation")
     })
-    @CrossOrigin(origins = "http://localhost:4200") // only allows access from our frontend
-    @GetMapping(value = "v2/clear")
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("clear")
     public void clear() {
         log.info("Clearing cache");
         adrService.clear();
+        importService.clear();
     }
 }
