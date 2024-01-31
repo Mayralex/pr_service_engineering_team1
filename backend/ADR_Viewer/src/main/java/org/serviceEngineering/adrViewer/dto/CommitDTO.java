23fd12d450771d3aaf5b809af506a9acdbf3fb12
juliaweinreich <54713794+juliaweinreich@users.noreply.github.com>
2024-01-31 16:22:36 +0100

    Update CommitDTO.java

--- backend/ADR_Viewer/src/main/java/org/serviceEngineering/adrViewer/dto/CommitDTO.java
+++ backend/ADR_Viewer/src/main/java/org/serviceEngineering/adrViewer/dto/CommitDTO.java
@@ -12,23 +12,3 @@ public class CommitDTO {
     private String committedDate;
     private String message;
 }
-
-/*@Getter
-@ToString
-public class CommitDTO {
-
-    private CommitData commit;
-
-    @Getter
-    public class CommitData {
-        private Commit commit;
-
-        @Getter
-        public class Commit {
-            private String oid;
-            private String committedDate;
-            private String message;
-        }
-    }
-
-}*/
